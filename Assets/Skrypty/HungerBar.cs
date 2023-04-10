274770acf957742fa263b30d071aa83acccdbedf
Czikbaum <mikbauch@gmail.com>
2023-04-10 22:57:34 +0200

    przyszedł kaszkowiak
    Przyszedł i chuj dalej nic nie zrobilim

--- Assets/Skrypty/HungerBar.cs
+++ Assets/Skrypty/HungerBar.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using System.Threading;
 public class HungerBar : MonoBehaviour
 {
 
-    public int Hunger = 100; 
+    public int Hunger = 100;
+    
+    public float delay = 50f;
+    float timer;
+
 
     Slider _hungerSlider;
 
     private void Start()
     {
+
+        
         _hungerSlider = GetComponent<Slider>();
+            
+
+      
     }
 
 
@@ -22,15 +31,19 @@ public class HungerBar : MonoBehaviour
         _hungerSlider.value = maxHunger;
     }
 
-    public void SetHunger(int hunger)
-    {
-        _hungerSlider.value = hunger;
-    }
+   
 
 
     public void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer > delay) 
+        {
+            Hunger--;
+            _hungerSlider.value = Hunger;
+        }
+
+
     }
 }
 
