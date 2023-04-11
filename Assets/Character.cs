3a607b5c1b246e6866570c9f7259ff14da6a63b9
Kelduin <kjaworowski@edu.cdv.pl>
2023-04-11 17:57:25 +0200

    cos tam dziala
    wkurwia mnie to unity nie chce mi sie tu robic czemu reference jest takie ciezkie do zrobienia w ue5 se po prostu przenosisz a tutaj kurwa musisz jakies kongo robic

---
+++ Assets/Character.cs
@@ -0,0 +1,77 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace SystemPostaci
+{
+
+    [System.Serializable]
+    public class PotrzebyContainer
+    {
+        [Range(0f, 100f)]
+        public float szybkoscPodnoszenia;
+        public float szybkoscSpadania = 1f;
+        public float state;
+        public Potrzeby TypPotrzeby;
+        public PotrzebyContainer(Potrzeby potrzeby)
+        {
+            TypPotrzeby = potrzeby;
+        }
+
+
+        public void Spadanie()
+        {
+            state -= szybkoscSpadania * Time.deltaTime;
+        }
+    }
+
+
+
+
+    public enum Potrzeby
+    {
+        Glod,
+        Pragnienie,
+        Zabawa,
+        Pecherz,
+        Higiena,
+        Energia,
+    }
+    public class Character : MonoBehaviour
+    {
+
+        public List<PotrzebyContainer> potrzeby;
+
+        public const int PotrzebyIlosc = 6;
+
+
+        [ContextMenu("Zacznij liste potrzeb")]
+
+
+        public void InitPotrzeby()
+        {
+            potrzeby = new List<PotrzebyContainer>();
+            for (int i = 0; i < PotrzebyIlosc; i++)
+            {
+                potrzeby.Add(new PotrzebyContainer((Potrzeby)i));
+            }
+        }
+
+
+        private void Update()
+        {
+            PotrzebySpadanie();
+            
+        }
+
+        private void PotrzebySpadanie()
+        {
+            for (int i = 0; i < PotrzebyIlosc; i++)
+            {
+                potrzeby[i].Spadanie();
+            }
+        }
+    }
+}
---
+++ Assets/PanelPotrzeb.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SystemPostaci;
+public class PanelPotrzeb : MonoBehaviour
+{
+  [SerializeField]  List<PotrzebyUI> children;
+  
+
+}
